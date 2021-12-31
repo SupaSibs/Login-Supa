28412cc7f7d18f39e151aaee3670c1b61606139b
SupaSibs <omareeto2012@hotmail.com>
2021-12-31 13:43:30 +0200

    HTML and css background
    got background to work from scss, tbh its easier than writing plain CSS
    cuz of its mixins, gonna start using them next project hopefully :) I
    also formatted the HTML to what it is now, going great so far.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+"use strict";
+const inputs = document.querySelectorAll(".inputs");
+const errors = document.querySelectorAll(".error");
+const submit = document.querySelector("#submit");
+function validate(inparray, input) {
+for (const element of inparray) {
+if (element.value === input.value) {
+error[indexOf(element)].style.display = "block";
+}
+else { 
+error[indexOf(element)].style.display = "block";
+alert("Success! Enjoy your free trial!")
+} } } 
